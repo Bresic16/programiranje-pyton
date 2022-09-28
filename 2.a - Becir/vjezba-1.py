d40b99ebdd71f456c9cbb8a10fd4e88debdf8cb6
Bresic16 <114572993+Bresic16@users.noreply.github.com>
2022-09-28 11:23:59 +0200

    Add files via upload

---
+++ 2.a - Becir/vjezba-1.py
@@ -0,0 +1,20 @@
+from cmath import pi
+
+
+print("Pozdrav brate")
+
+print("ucimo phyton!")
+
+ime= input("Kako se zoveš?")
+
+print("Pozdrav, " + ime)
+
+x=10
+
+pi =3.14
+
+print(pi)
+
+poruka="Ovo su znakovi - Stringovi"
+
+print(poruka)
